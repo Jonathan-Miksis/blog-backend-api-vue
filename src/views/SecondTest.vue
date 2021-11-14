8f79d15bbadac2c4cd4190a913e1943d06f2f354
Jonathan-Miksis <jmiksis13@gmail.com>
2021-11-14 11:34:10 -0500

    add second test page

---
+++ src/views/SecondTest.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="second-test">
+    <h1>{{ message }}</h1>
+  </div>
+</template>
+
+<style></style>
+
+<script>
+  export default {
+    data: function () {
+      return {
+        message: "Welcome to the Second Test Page!",
+      };
+    },
+    created: function () {},
+    methods: {},
+  };
+</script>
