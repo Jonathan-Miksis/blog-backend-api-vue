e2e47581bf2cd5af5d7cbbf7654873b1e9af41b4
Jonathan-Miksis <jmiksis13@gmail.com>
2021-11-14 11:30:04 -0500

    add separate test page with link for user to navigate

---
+++ src/views/Test.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="test">
+    <h1>{{ message }}</h1>
+  </div>
+</template>
+
+<style></style>
+
+<script>
+  export default {
+    data: function () {
+      return {
+        message: "Welcome to the Test Page!",
+      };
+    },
+    created: function () {},
+    methods: {},
+  };
+</script>
