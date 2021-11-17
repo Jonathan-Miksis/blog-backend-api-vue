76fb0fe2bfad7f24a29af45ba0055dd124f1642b
Jonathan-Miksis <jmiksis13@gmail.com>
2021-11-16 20:51:02 -0500

    add bootstrap to index page, navbar, grid, cards

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <div id="nav">
+    <!-- <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link> |
       <router-link to="/posts">Posts</router-link> |
@@ -8,30 +8,46 @@
       <router-link to="signup">Signup</router-link> |
       <router-link to="login">Login</router-link> |
       <router-link to="logout">Logout</router-link> |
+    </div> -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="/posts">ArtWorld</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/posts/new">New Art Post</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Authentication
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="/login">Login</a></li>
+            <li><a class="dropdown-item" href="/logout">Signout</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="/signup">Signup</a></li>
+          </ul>
+        </li>
+      </ul>
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
     </div>
+  </div>
+</nav>
     <router-view/>
   </div>
 </template>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  font-family: 'Andale Mono','Lucida Grande', Verdana, sans-serif;
+  background-image: url('./assets/chevron.png');
   text-align: center;
-  color: #2c3e50;
 }
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
--- src/views/PostsIndex.vue
+++ src/views/PostsIndex.vue
@@ -1,12 +1,24 @@
 <template>
   <div class="posts-index">
+    <div class="container">
     <h1>{{ message }}</h1>
-    <div v-for="post in posts">
+    <!-- <div v-for="post in posts">
       <p>Title: {{post.title}}</p>
       <router-link v-bind:to="`/posts/${post.id}`">
-      <img v-bind:src="post.image">
-      </router-link>
-      <hr>
+      <img v-bind:src="post.image"> -->
+      <div class="row">
+        <div class="col-sm-6" v-for="post in posts">
+          <div class="card text-center" style="width: 18rem;">
+            <img v-bind:src="post.image" class="card-img-top" v-bind:alt="post.title">
+            <div class="card-body">
+              <h5 class="card-title">{{ post.title }}</h5>
+              <a v-bind:href="`/posts/${post.id}`" class="btn btn-primary">More about this Artwork</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- </router-link>
+      <hr> -->
     </div>
   </div>
 </template>
@@ -19,7 +31,7 @@ import axios from 'axios'
   export default {
     data: function () {
       return {
-        message: "Welcome to Posts!",
+        message: "Welcome to all of the Art!",
         posts: [],
       };
     },
